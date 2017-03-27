5591ad9af25101088040e2178d2eb9c74765509b
GabrielAlacchi <alacchi.g@gmail.com>
2017-03-27 12:31:21 -0400

    Configured browsersync and npm scripts

--- gulpfile.js
+++ gulpfile.js
@@ -5,35 +5,53 @@ var sass = require('gulp-sass');
 var rename = require('gulp-rename');
 var autoprefixer = require('gulp-autoprefixer');
 
+var browserSync = require('browser-sync').create();
+
 gulp.task('scripts', function() {
-    return gulp.src('js/scripts.js')
-        .pipe(plumber(plumber({
-            errorHandler: function (err) {
-                console.log(err);
-                this.emit('end');
-            }
-        })))
-        .pipe(uglify({
-            preserveComments: 'license'
-        }))
-        .pipe(rename({extname: '.min.js'}))
-        .pipe(gulp.dest('js'));
+  return gulp.src('js/scripts.js')
+    .pipe(plumber(plumber({
+      errorHandler: function (err) {
+        console.log(err);
+        this.emit('end');
+      }
+    })))
+    .pipe(uglify({
+      preserveComments: 'license'
+    }))
+    .pipe(rename({extname: '.min.js'}))
+    .pipe(gulp.dest('js'))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('styles', function() {
-    return gulp.src('scss/styles.scss')
-        .pipe(plumber(plumber({
-            errorHandler: function (err) {
-                console.log(err);
-                this.emit('end');
-            }
-        })))
-        .pipe(sass({outputStyle: 'compressed'}))
-        .pipe(autoprefixer())
-        .pipe(gulp.dest('css'));
+  return gulp.src('scss/styles.scss')
+    .pipe(plumber(plumber({
+      errorHandler: function (err) {
+        console.log(err);
+        this.emit('end');
+      }
+    })))
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('css'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('update', function() {
+  return gulp.src('*.html')
+    .pipe(browserSync.stream());
 });
 
 gulp.task('watch', ['scripts', 'styles'], function() {
-    gulp.watch('js/*.js', ['scripts']);
-    gulp.watch('scss/*.scss', ['styles']);
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+
+  gulp.watch('js/*.js', ['scripts']);
+  gulp.watch('scss/*.scss', ['styles']);
+  gulp.watch('*.html', ['update']);
 });
+
+gulp.task('default', ['scripts', 'styles', 'watch']);
